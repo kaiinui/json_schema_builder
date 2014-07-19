098ff19a7d166f8667d1dec766ec55c5b151a29c
Kai INUI <lied.der.optik@gmail.com>
2014-07-19 17:18:13 +0900

    model->properties

--- lib/json_schema_builder.rb
+++ lib/json_schema_builder.rb
@@ -1,5 +1,3 @@
-
-
 class JSONSchemaBuilder
   class << self
     def build_from_schema(schema)
@@ -18,15 +16,18 @@ class JSONSchemaBuilder
             ],
             definitions: model.properties.map do |property|
               {property.name => property.options}
-            end
+            end,
+            links: [],
+            properties: []
         }
-        json[:links] = []
 
+        json[:definitions][model.name][:links] = []
+        # [TODO]: Refactor
         [
             {method: "GET", rel: "index"},
             {method: "POST", rel: "create"}
         ].each do |link|
-          json[:links].push({
+          json[:definitions][model.name][:links].push({
                                 description: "#{link[:rel]} #{model.name}.",
                                 href: "/#{model.plural_name}",
                                 method: link[:method],
@@ -35,18 +36,27 @@ class JSONSchemaBuilder
           })
         end
 
+        # [TODO]: Refactor
         [
             {method: "GET", rel: "show"},
             {method: "PATCH", rel: "update"},
             {method: "DELETE", rel: "destroy"}
         ].each do |link|
-          json[:links].push({
+          json[:definitions][model.name][:links].push({
                                 description: "#{link[:rel]} #{model.name}.",
                                 href: "/#{model.plural_name}/{#/definitions/#{model.name}/id}",
                                 method: link[:method],
                                 rel: link[:rel],
                                 title: link[:rel]
           })
+        end
+
+        json[:definitions][model.name][:properties] = model.properties.map do |property|
+          {
+              id: {
+                  "$ref" => "#/definitions/#{model.name}/definitions/#{property.name}"
+              }
+          }
         end
       end
 
