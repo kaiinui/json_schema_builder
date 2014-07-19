019f128bcaed5a1155c034c7604fa171946dd5f3
Kai INUI <lied.der.optik@gmail.com>
2014-07-19 15:51:35 +0900

    setup rspe

--- check.rb
+++ check.rb
@@ -1,8 +1,10 @@
 require_relative 'lib/schema_builder'
 
-SchemaBuilder.build do
+schema = SchemaBuilder.build do
   model :app do
-    property :id
-    property :name
+    property :id, description: "some description", example: "1", format: "UUID", read_only: "true", type: "String"
+    property :name, description: "some description", example: "1", pattern: "^[a-z][a-z0-9-]{3,50}$", read_only: "true", type: "String"
   end
 end
+
+puts schema.models[0].properties[0].options
---
+++ spec/lib/schema_builder_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../../spec/spec_helper'
+
+describe SchemaBuilder do
+  context '#build' do
+    let(:schema) do
+      SchemaBuilder.build do
+        model :app do
+          property :id, description: "some description", example: "1", format: "UUID", read_only: "true", type: "String"
+          property :name, description: "some description", example: "1", pattern: "^[a-z][a-z0-9-]{3,50}$", read_only: "true", type: "String"
+        end
+      end
+    end
+
+    let(:model_app) { schema.models.first }
+
+    it { expect(model_app.properties.size).to eq 2 }
+    it { expect(model_app.properties.first.name).to eq :id }
+  end
+end
